a73e1b6faee572a94f7ae72b5ea3017f2654b6b3
erin nicole johnson <erin.nicole.johnson@gmail.com>
2020-06-04 17:05:23 +0100

    update background

--- js-scripts.js
+++ js-scripts.js
@@ -3,15 +3,13 @@ let servicesImageDivs = document.querySelectorAll('.services-image');
 
 for (let i = 0; i < servicesImageDivs.length; i++) {
     servicesImageDivs[i].addEventListener("mouseover", function() {
-        servicesImageDivs[i].style['background-color'] = "#aaa";
-        servicesImageDivs[i].style['background-blend-mode'] = "color-burn";
+        servicesImageDivs[i].classList.add('darker-background');
         let description = servicesImageDivs[i].querySelector('.services-desc')
         description.classList.remove('hide');
         description.classList.add('show');        
     });
     servicesImageDivs[i].addEventListener("mouseout", function() {
-        servicesImageDivs[i].style['background-color'] = 'inherit';
-        servicesImageDivs[i].style['background-blend-mode'] = "initial";
+        servicesImageDivs[i].classList.remove('darker-background');
         let description = servicesImageDivs[i].querySelector('.services-desc')
         description.classList.remove('show');
         description.classList.add('hide');        
